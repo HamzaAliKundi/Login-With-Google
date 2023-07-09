68809334c1edc943c74071387597f0df44d1993a
HamzaAliKUndi <hamza.alee83@gmail.com>
2023-07-09 11:32:32 +0500

    Login-With-Google

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { gapi } from "gapi-script";
+import { refreshTokenSetup } from "./refreshTokenSetup";
+import GoogleLogin from "react-google-login";
+import jwtDecode from "jwt-decode";
 
 function App() {
+  gapi.load("client:auth2", () => {
+    gapi.client.init({
+      clientId: "*****.apps.googleusercontent.com",
+      plugin_name: "chat",
+    });
+  });
+
+  const responseSuccessGoogle = (success) => {
+    const token = jwtDecode(success.tokenObj.id_token);
+
+    const userObj = {
+      name: token.name,
+      email: token.email,
+    };
+
+    console.log("User Obj : ", userObj);
+  };
+
+  const responseFailureGoogle = (failure) => {
+    console.log("first failure : ", failure);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "purple",
+        }}
+      >
+        <div>
+          <GoogleLogin
+            clientId="340636402788-k287jmsa8kbmuhb3gi3ifdtg054vfu6l.apps.googleusercontent.com"
+            buttonText="Login With Google"
+            onSuccess={responseSuccessGoogle}
+            onFailure={responseFailureGoogle}
+            cookiePolicy={"single_host_origin"}
+            refreshTokenSetup={refreshTokenSetup}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
